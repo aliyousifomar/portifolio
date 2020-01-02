0b85524ad0e75bbce457eda92c903a4f5627d4e5
Ali Omar <ali.yousif1987@gmail.com>
2020-01-02 13:00:17 +0300

    databases added with postgres

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'portifolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #linked the project to postgresql database with all the parameters and credentials
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Star1987',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
