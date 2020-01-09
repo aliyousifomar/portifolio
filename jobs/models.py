0f7f658dcadef1cb6957c1df4e12b69aedf9c1e0
Ali Omar <ali.yousif1987@gmail.com>
2020-01-09 11:26:02 +0300

    have modified home.html

--- jobs/models.py
+++ jobs/models.py
@@ -4,5 +4,7 @@ class Job (models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
+
+    # This method will put the object's name/title on admin page!
     def __str__(self):
         return self.summary[:100]
