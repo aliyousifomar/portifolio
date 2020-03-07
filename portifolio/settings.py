da04e6aa9a81502d0fd60ac494461dedc47163f0
Ali Omar <ali.yousif1987@gmail.com>
2020-03-07 17:28:46 +0300

    static files directory created within setting.py

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # Added the path of media files by me
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portifolio/static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # added the path to the 
